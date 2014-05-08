7aeba698895c5f95c61e4752d7e8c821cf038739
Tim Kurvers <tim@moonsphere.net>
2014-05-08 14:00:43 +0200

    Not all Rails projects have migrations

--- lib/heroku/command/deploy/packs/rails.rb
+++ lib/heroku/command/deploy/packs/rails.rb
@@ -4,9 +4,17 @@ class Heroku::Command::Deploy::Pack::Rails < Heroku::Command::Deploy::Pack
   def deploy!(branch)
     heroku 'maintenance:on'
     run "git push #{remote} #{branch}:master"
-    heroku_run 'rake db:migrate'
-    heroku 'ps:restart'
+
+    if migrations?
+      heroku_run 'rake db:migrate'
+      heroku 'ps:restart'
+    end
+
     heroku 'maintenance:off'
+  end
+
+  def migrations?
+    File.exists? 'db/migrate'
   end
 
 end
