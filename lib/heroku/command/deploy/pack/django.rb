739218a5d977853b842a22933ad8e7d5ccc86c80
Tim Kurvers <tim@moonsphere.net>
2014-10-08 14:56:43 +0200

    Modularize pack detection and handle edge-cases

--- lib/heroku/command/deploy/pack/django.rb
+++ lib/heroku/command/deploy/pack/django.rb
@@ -1,6 +1,14 @@
 # Django deploy pack
 class Heroku::Command::Deploy::Pack::Django < Heroku::Command::Deploy::Pack
 
+  class << self
+
+    def applicable?(dir = Dir.pwd)
+      File.exist? File.join(dir, 'requirements.txt')
+    end
+
+  end
+
   def deploy!(branch)
     maintenance :on
     push branch
--- lib/heroku/command/deploy/pack/rails.rb
+++ lib/heroku/command/deploy/pack/rails.rb
@@ -1,6 +1,14 @@
 # Ruby on Rails deploy pack
 class Heroku::Command::Deploy::Pack::Rails < Heroku::Command::Deploy::Pack
 
+  class << self
+
+    def applicable?(dir = Dir.pwd)
+      File.exist? File.join(dir, 'Gemfile')
+    end
+
+  end
+
   def deploy!(branch)
     maintenance :on
     push branch
