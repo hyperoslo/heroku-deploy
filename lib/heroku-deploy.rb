89da3c951084787803d1ad266bbc9d922478b5cf
Tim Kurvers <tim@moonsphere.net>
2014-10-08 11:30:37 +0200

    Move string extensions into core_ext folder

--- lib/heroku-deploy.rb
+++ lib/heroku-deploy.rb
@@ -1,6 +1,6 @@
 require 'heroku/command/deploy'
+require 'heroku/command/deploy/core_ext/string'
 require 'heroku/command/deploy/helpers'
 require 'heroku/command/deploy/pack'
 require 'heroku/command/deploy/pack/rails'
 require 'heroku/command/deploy/pack/django'
-require 'heroku/command/deploy/string_extensions'
